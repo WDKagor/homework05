f117fde81c0db21277914cbbc041cf00b017c402
briz1 <my1queen8my1king@gmail.com>
2023-12-18 13:21:17 +0700

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Задача 1");
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else System.out.println("Установите версию приложения для Android по ссылке");
+
+        System.out.println("   ");
+        System.out.println("Задача 2");
+        int clientDeviceYear = 2014;
+        if (clientDeviceYear < 2015) {
+            if (clientOS == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else System.out.println("Установите версию приложения для Android по ссылке");
     }
+
+
+
+
+
 }
